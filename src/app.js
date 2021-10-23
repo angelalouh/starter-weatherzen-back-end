b43928d57517ca54243563e34ff5d580d6cbb75a
Angela Louh <louh.angela@gmail.com>
2021-10-22 23:40:33 -0700

    Added the observationsRouter and "/observations" route

--- src/app.js
+++ src/app.js
@@ -4,21 +4,25 @@ const cors = require("cors");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
 
+const observationsRouter = require('./observations/observations.router')
+
 const app = express();
 
 app.use(cors())
 app.use(express.json());
 
-let nextId = 1;
-app.post('/observations', ((req, res) => {
-    const newObservation = req.body.data;
+// let nextId = 1;
+// app.post('/observations', ((req, res) => {
+//     const newObservation = req.body.data;
 
-    newObservation.observation_id = nextId++
+//     newObservation.observation_id = nextId++
 
-    res.status(201).json({
-        data: newObservation,
-    });
-}))
+//     res.status(201).json({
+//         data: newObservation,
+//     });
+// }))
+
+app.use("/observations", observationsRouter);
 
 app.use(notFound);
 app.use(errorHandler);
